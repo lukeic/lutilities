2227567f087fef48bf3d3659da1b6785c8d9e5b9
Luke Ivanchenko-Cooney <lukeic@users.noreply.github.com>
2018-10-24 06:47:02 +1100

    Add Wrap

--- pkg/errors/errors.go
+++ pkg/errors/errors.go
@@ -2,12 +2,16 @@ package errors
 
 import "github.com/pkg/errors"
 
+func New(msg string) error {
+  return errors.New(msg)
+}
+
+func Wrap(err error, msg string) error {
+  return errors.Wrap(err, msg)
+}
+
 func Wrapped(msg string) func(err error) error {
   return func(err error) error {
     return errors.Wrap(err, msg)
   }
-}
-
-func New(msg string) error {
-  return errors.New(msg)
 }
